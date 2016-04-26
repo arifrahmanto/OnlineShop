e23a6bd1bcf969379f4849732e851e40f9e959f3
Arif <ArifIsmiadi.Rahmanto@mitrais.com>
2016-04-26 14:01:13 +0700

    Insert Product

--- README1.md
+++
@@ -1,2 +0,0 @@
-# OnlineShop
-Learn MEAN Stack
---
+++ modules/products/server/models/product.server.model.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Product Schema
+ */
+var ProductSchema = new Schema({
+  title: {
+    type: String,
+    default: '',
+    required: 'Please fill Product name',
+    trim: true
+  },
+  author: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  description: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  category: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  imageUrl: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  price: {
+    type: Number,
+    default: 0
+  },
+  stock: {
+    type: Number,
+    default: 0
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+mongoose.model('Product', ProductSchema);
