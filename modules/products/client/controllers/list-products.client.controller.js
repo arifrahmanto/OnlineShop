4c9d9f837be664dad98c8d86602efd3f65a09183
Arif <ArifIsmiadi.Rahmanto@mitrais.com>
2016-04-27 15:23:24 +0700

    Search Product

--- modules/products/client/controllers/list-products.client.controller.js
+++ modules/products/client/controllers/list-products.client.controller.js
@@ -5,12 +5,15 @@
   .module('products')
   .controller('ProductsListController', ProductsListController);
 
-  ProductsListController.$inject = ['ProductsService'];
+  ProductsListController.$inject = ['ProductsService','$location'];
 
-  function ProductsListController(ProductsService) {
+  function ProductsListController(ProductsService, $location) {
     var vm = this;
     vm.curPage = 1;
     vm.pageSize = 4;
+    var searchValue = $location.search();
+    vm.searchText = searchValue.search;
+
     var query = ProductsService.query();
     query.$promise.then(function(data){
       vm.products = data;
